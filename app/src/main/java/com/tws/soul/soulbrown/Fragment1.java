8b2fd393eb67c4e8cf379f82ddd46fe266da27ba
Jony <jony80@naver.com>
2015-01-04 22:52:38 +0900

    init

---
+++ app/src/main/java/com/tws/soul/soulbrown/Fragment1.java
@@ -0,0 +1,17 @@
+package com.tws.soul.soulbrown;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_soul_brown_main, container, false);
+		return rootView;
+	}
+	
+}
