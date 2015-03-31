af958c7481eff36cef3991c4a5ce5bc9f5bb01ee
Jony <jony80@naver.com>
2015-03-31 23:33:27 +0900

    음료 10잔으로 제한.

--- app/src/main/java/com/tws/common/listview/adapter/MenuListAdapter.java
+++ app/src/main/java/com/tws/common/listview/adapter/MenuListAdapter.java
@@ -15,10 +15,12 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.tws.common.lib.views.CuzToast;
 import com.tws.network.data.ArrayOptionData;
 import com.tws.soul.soulbrown.R;
 import com.tws.soul.soulbrown.data.Menu;
@@ -32,14 +34,14 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 /**
  * Created by Jony on 2015-01-11.
  */
-public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHolder>{
+public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHolder> {
 
 
     // Activity 로 데이터를 전달할 커스텀 리스너
     private CuzOnClickListener customListener;
 
     // Activity 로 데이터를 전달할 커스텀 리스너의 인터페이스
-    public interface CuzOnClickListener{
+    public interface CuzOnClickListener {
         public void onChangeItem(List<Menu> menu);
     }
 
@@ -47,8 +49,11 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
     private int rowLayout;
     private Context mContext;
 
+    private int mSumCnt = 0;
+
     private int itemCount = 0;
     private BitmapPool mPool;
+    private CuzToast mCuzToast;
 
     public MenuListAdapter(List<Menu> listMenu, int rowLayout, Context context, CuzOnClickListener listener) {
         this.listMenu = listMenu;
@@ -56,6 +61,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
         this.mContext = context;
         this.customListener = listener;
         this.mPool = Glide.get(mContext).getBitmapPool();
+        mCuzToast = new CuzToast(context);
     }
 
     @Override
@@ -74,12 +80,11 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
         // option S
 
 
-        if( listMenu.get(num).option != null) {
+        if (listMenu.get(num).option != null) {
 
             int cntOpt = listMenu.get(num).option.size();
 
-            if( cntOpt == 1)
-            {
+            if (cntOpt == 1) {
                 viewHolder.menuCountOpt01.setText(Integer.toString(listMenu.get(num).option.get(0).count));
 
                 viewHolder.menuLayoutOpt01.setVisibility(View.VISIBLE);
@@ -88,18 +93,13 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int addPrice = listMenu.get(num).option.get(0).addprice;
 
-                if( addPrice != 0)
-                {
-                    viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name + " +"+addPrice+"원");
-                }
-                else
-                {
+                if (addPrice != 0) {
+                    viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name + " +" + addPrice + "원");
+                } else {
                     viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name);
                 }
 
-            }
-            else
-            {
+            } else {
                 viewHolder.menuCountOpt01.setText(Integer.toString(listMenu.get(num).option.get(0).count));
                 viewHolder.menuCountOpt02.setText(Integer.toString(listMenu.get(num).option.get(1).count));
 
@@ -109,23 +109,17 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int addPrice = listMenu.get(num).option.get(0).addprice;
 
-                if( addPrice != 0)
-                {
-                    viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name + " +"+addPrice+"원");
-                }
-                else
-                {
+                if (addPrice != 0) {
+                    viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name + " +" + addPrice + "원");
+                } else {
                     viewHolder.menuTextOpt01.setText(listMenu.get(num).option.get(0).name);
                 }
 
                 addPrice = listMenu.get(num).option.get(1).addprice;
 
-                if( addPrice != 0)
-                {
-                    viewHolder.menuTextOpt02.setText(listMenu.get(num).option.get(1).name + " +"+addPrice+"원");
-                }
-                else
-                {
+                if (addPrice != 0) {
+                    viewHolder.menuTextOpt02.setText(listMenu.get(num).option.get(1).name + " +" + addPrice + "원");
+                } else {
                     viewHolder.menuTextOpt02.setText(listMenu.get(num).option.get(1).name);
                 }
 
@@ -140,8 +134,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
         int price = menu.price;
         int saleprice = menu.saleprice;
 
-        if( price != saleprice)
-        {
+        if (price != saleprice) {
 
             viewHolder.menuPrice.setTextColor(Color.parseColor("#E52A19"));
             viewHolder.menuPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
@@ -150,8 +143,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
             viewHolder.menuSalePrice.setText(ConvertData.getPrice(menu.saleprice));
             viewHolder.menuArrow.setVisibility(View.VISIBLE);
 
-        }
-        else {
+        } else {
             viewHolder.menuPrice.setTextColor(Color.parseColor("#3D454C"));
             viewHolder.menuPrice.setText(ConvertData.getPrice(menu.price));
             viewHolder.menuSalePrice.setVisibility(View.GONE);
@@ -171,7 +163,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
             @Override
             public void onClick(View v) {
 
-                Log.i("jony","imageUrl : "+ imageUrl);
+                Log.i("jony", "imageUrl : " + imageUrl);
 
                 sendSlidingImage(imageUrl);
             }
@@ -187,17 +179,28 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int cnt = listMenu.get(num).option.get(0).count;
 
-                if(cnt < 10) {
-                    cnt++;
 
-                    cntViewOpt01.setText(Integer.toString(cnt));
+                if (cnt < 10) {
+                    if (mSumCnt < 10) {
+                        mSumCnt++;
+                        cnt++;
 
-                    listMenu.get(num).option.get(0).count = cnt;
+                        cntViewOpt01.setText(Integer.toString(cnt));
 
-                    customListener.onChangeItem(listMenu);
+
+                        listMenu.get(num).option.get(0).count = cnt;
+
+                        customListener.onChangeItem(listMenu);
+                    } else {
+                        mCuzToast.showToast(mContext.getString(R.string.max_item), Toast.LENGTH_SHORT);
+                    }
+                } else {
+                    mCuzToast.showToast(mContext.getString(R.string.max_item), Toast.LENGTH_SHORT);
+
                 }
 
-                Log.i("jony"," onClick menuAdd ");
+
+                Log.i("jony", " onClick menuAdd ");
 
             }
         });
@@ -208,18 +211,26 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int cnt = listMenu.get(num).option.get(0).count;
 
-                if(cnt > 0) {
-                    cnt--;
 
-                    cntViewOpt01.setText(Integer.toString(cnt));
+                if (cnt > 0) {
 
-                    listMenu.get(num).option.get(0).count = cnt;
+                    if (mSumCnt > 0) {
+                        mSumCnt--;
+                        cnt--;
 
-                    customListener.onChangeItem(listMenu);
+
+                        cntViewOpt01.setText(Integer.toString(cnt));
+
+                        listMenu.get(num).option.get(0).count = cnt;
+
+                        customListener.onChangeItem(listMenu);
+                    } else {
+                    }
+
                 }
 
 
-                Log.i("jony"," onClick menuRemove ");
+                Log.i("jony", " onClick menuRemove ");
 
             }
         });
@@ -230,17 +241,27 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int cnt = listMenu.get(num).option.get(1).count;
 
-                if(cnt < 10) {
-                    cnt++;
 
-                    cntViewOpt02.setText(Integer.toString(cnt));
+                if (cnt < 10) {
+                    if (mSumCnt < 10) {
+                        mSumCnt++;
+                        cnt++;
 
-                    listMenu.get(num).option.get(1).count = cnt;
+                        cntViewOpt02.setText(Integer.toString(cnt));
 
-                    customListener.onChangeItem(listMenu);
+                        listMenu.get(num).option.get(1).count = cnt;
+
+                        customListener.onChangeItem(listMenu);
+                    } else {
+                        mCuzToast.showToast(mContext.getString(R.string.max_item), Toast.LENGTH_SHORT);
+                    }
+                } else {
+                    mCuzToast.showToast(mContext.getString(R.string.max_item), Toast.LENGTH_SHORT);
+
                 }
 
-                Log.i("jony"," onClick menuAdd ");
+
+                Log.i("jony", " onClick menuAdd ");
 
             }
         });
@@ -251,24 +272,27 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
                 int cnt = listMenu.get(num).option.get(1).count;
 
-                if(cnt > 0) {
-                    cnt--;
 
-                    cntViewOpt02.setText(Integer.toString(cnt));
+                if (cnt > 0) {
+                    if (mSumCnt > 0) {
+                        mSumCnt--;
+                        cnt--;
 
-                    listMenu.get(num).option.get(1).count = cnt;
+                        cntViewOpt02.setText(Integer.toString(cnt));
 
-                    customListener.onChangeItem(listMenu);
+                        listMenu.get(num).option.get(1).count = cnt;
+
+                        customListener.onChangeItem(listMenu);
+                    } else {
+
+                    }
                 }
 
 
-                Log.i("jony"," onClick menuRemove ");
+                Log.i("jony", " onClick menuRemove ");
 
             }
         });
-
-
-
 
 
     }
@@ -307,7 +331,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
             menuName = (TextView) itemView.findViewById(R.id.item_menu_info_name);
             menuPrice = (TextView) itemView.findViewById(R.id.item_menu_info_price);
             menuSalePrice = (TextView) itemView.findViewById(R.id.item_menu_info_saleprice);
-            menuImage = (ImageView)itemView.findViewById(R.id.item_menu_info_image);
+            menuImage = (ImageView) itemView.findViewById(R.id.item_menu_info_image);
 
             // option
 
@@ -330,8 +354,7 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.ViewHo
 
     }
 
-    private void sendSlidingImage(String url)
-    {
+    private void sendSlidingImage(String url) {
         Intent intentSliding = new Intent("image_url");
 
         intentSliding.putExtra("url", url);
