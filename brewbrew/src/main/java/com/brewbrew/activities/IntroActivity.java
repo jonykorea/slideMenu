b40a03c9b169b43f7900f0d398291190d949876d
Jony <jony80@naver.com>
2015-06-20 00:46:33 +0900

    mod01 commit

--- brewbrew/src/main/java/com/brewbrew/activities/IntroActivity.java
+++ brewbrew/src/main/java/com/brewbrew/activities/IntroActivity.java
@@ -16,11 +16,6 @@ public class IntroActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_intro);
-
-
     }
-
-
 }
